9ffca4b3dd696373b0ce7a1fbd9f46f39e73fc3c
Mariusz Rukowicz <mariuszrukowicz@wp.pl>
2020-06-30 11:12:18 +0200

    feat: add macy gallery

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,18 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀');
+
+const masonry = new Macy({
+  container: '.container',
+  mobileFirst: true,
+  colums: 1,
+  breakAt: {
+    320: 1,
+    700: 2,
+    1100: 3,
+  },
+  margin: {
+    x: 20,
+    y: 20,
+  },
+});
